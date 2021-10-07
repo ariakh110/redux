eec346eda1387a21e0c364e6c3dfee925868933f
ariakhayer <ariakh110@gmail.com>
2021-10-07 16:32:02 +0330

    v-26

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPost } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends Component {
   componentDidMount() {
     console.log(this.props);
-    
+
     this.props.fetchPost();
   }
   renderList() {
-    return (this.props.posts.map((post) => {
+    return this.props.posts.map((post) => {
       return (
         <div className="item" key={post.id}>
-          <i className="larg middle aligned icon user"/>
+          <i className="larg middle aligned icon user" />
           <div className="content">
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
-      )       
-    }))
+      );
+    });
   }
   render() {
-    return( <div className="ui relaxed divided list">{this.renderList()}</div> );
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
-const mapStateToProps = (state) =>{
-  return {posts: state.posts}
-}
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
 export default connect(mapStateToProps, { fetchPost })(PostList);
---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserHeader extends Component {
+    render() {
+        
+        return (
+            <div>
+                UserHeader
+            </div>
+        );
+    }
+}
+
+export default UserHeader;
