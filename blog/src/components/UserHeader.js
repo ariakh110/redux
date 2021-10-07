0a5d8024e7a361db6ad2e42137eba69680bb9566
ariakhayer <ariakh110@gmail.com>
2021-10-07 17:06:45 +0330

    v-28

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -7,16 +7,17 @@ class UserHeader extends Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find(
-      (user) => user.id === this.props.userId
-    );
+    const user = this.props
+    //   (user) => user.id === this.props.userId
+    // );
     if (!user) {
       return null;
     }
+
     return <div className="header">{user.name}</div>;
   }
 }
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { users: state.users.find((user) => user.id === ownProps.userId) };
 };
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
