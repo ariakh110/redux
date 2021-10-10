58220f5dfa1e88c5304fb7124a5e930abf8fb210
ariakhayer <ariakh110@gmail.com>
2021-10-10 13:13:45 +0330

    v-34

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Pageone = () => {
+  return <> page 1</>;
+};
+const Pagetwo = () => {
+  return <> page 2</>;
+};
+
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Route path="/" component={Pageone} exact />
+        <Route path="/about" component={Pagetwo} />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
