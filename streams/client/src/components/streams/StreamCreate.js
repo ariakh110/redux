e116771f60cfdbb8fa14b95f61a84dc6ec0792d6
ariakhayer <ariakh110@gmail.com>
2021-10-29 18:15:14 +0330

    v-58 form validation

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
+    console.log(meta);
     //can be {input} insted of formProps
     return (
       <div className="field">
@@ -9,22 +10,44 @@ class StreamCreate extends Component {
         {/* then clear formProps -> {...input} */}
         {/* <input {...formProps.input} /> */}
         <input {...input} />
+        <div>
+            {meta.error}
+        </div>
       </div>
     );
   }
-
+  onSubmit(formValues) {
+    //   console.log(formValues);
+  }
   render() {
     console.log();
     return (
       <>
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+        >
           <Field name="title" component={this.renderInput} label="Title" />
-          <Field name="description"component={this.renderInput}label="Description" />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Description"
+          />
           <button className="button ui primary">submit</button>
         </form>
       </>
     );
   }
 }
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "enter title";
+  }
+  if (!formValues.description) {
+    errors.description = "enter description";
+  }
 
-export default reduxForm({ form: "streamCreate" })(StreamCreate);
+  return errors;
+};
+export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
