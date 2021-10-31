03acfa3450922429ffa13677ec9b66d78967922e
ariakhayer <ariakh110@gmail.com>
2021-10-31 16:32:10 +0330

    v-71 create {create  btn}

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends Component {
@@ -30,12 +31,23 @@ class StreamList extends Component {
       );
     });
   };
-
+  renderCreate = () => {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button green">
+            create new stream
+          </Link>
+        </div>
+      );
+    }
+  };
   render() {
     return (
       <div>
         <h2>streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -46,6 +58,7 @@ const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
