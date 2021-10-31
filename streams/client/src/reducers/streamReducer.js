bd507af16a2fbad66b6a90b9ac5c2c22ff1219b3
ariakhayer <ariakh110@gmail.com>
2021-10-31 12:28:11 +0330

    v-67 { refine delete reducer and create list streams}

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 import {
   CREAT_STREAM,
   FETCH_STREAMS,
@@ -7,21 +7,20 @@ import {
   DELETE_STREAM,
 } from "../actions/types";
 
-const streamReducer =  (state={}, action) => {
-    switch (action.type) {
-        case FETCH_STREAM:
-            return {...state,[action.payload.id]: action.payload};
-        case CREAT_STREAM:
-            return {...state,[action.payload.id]: action.payload};
-        case EDIT_STREAM:
-            return {...state,[action.payload.id]: action.payload};
-
-        case DELETE_STREAM:
-            //
-            return {...state}
-        default:
-            return state;
-          
-    }
-}
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREAT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state,action.id);
+    default:
+      return state;
+  }
+};
 export default streamReducer;
