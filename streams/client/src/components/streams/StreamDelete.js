5894cdd5e062c04aace6c1fc47c448402732f874
ariakhayer <ariakh110@gmail.com>
2021-11-01 21:32:13 +0330

    v-82 dynamic modal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import Modal from '../Modal'
-
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
 export default function StreamDelete() {
-    return (
-        <div>
-            <Modal></Modal>
-        </div>
-    )
+  const actions = (
+    <>
+      <button className="ui red button">Delete</button>
+      <button className="ui cancel button" onClick={(e) => history.push("/")}>Cancel</button>
+    </>
+  );
+
+  return (
+    <div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete?"
+        actions={actions}
+      />
+    </div>
+  );
 }
