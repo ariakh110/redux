b7e58fc1a2f3d56472c4a2a934083f30c6acd061
ariakhayer <ariakh110@gmail.com>
2021-11-01 18:27:40 +0330

    v-81 create delete page

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import history from "../history";
 const Modal = (props) => {
-    return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                sdfadsfsdfasdfassd
-                fa fasdfasdfsdfasfsdf
-                sdfsdfasdfasdfasdf
-            </div>
-        </div>,
-        document.getElementById('modal')
-    )
-}
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
+      <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
+        <div className="header">Delete Stream </div>
+        <div className="content">
+            are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+            <button className="ui red button" >Delete</button>
+            <button className="ui cancel button" onClick={(e) => history.push('/')}>Cancel</button>
+        </div>
+      </div>
+    </div>,
+    document.getElementById("modal")
+  );
+};
 
-export default Modal
+export default Modal;
