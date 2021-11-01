45713909d9e983f838693f6672866e480e60845c
ariakhayer <ariakh110@gmail.com>
2021-11-01 17:40:11 +0330

    v-78 use streamform in streamedit and fix mass assginment

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
-
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from './StreamForm';
+import _ from 'lodash';
+import streams from '../../apis/streams';
 class StreamEdit extends Component {
-    componentDidMount() {
+    componentDidM2ount() {
         this.props.fetchStream(this.props.match.params.id);
     }
-    
+    onSubmit = (formValues) => {
+
+    }
     render() {
         if (!this.props.stream) {
             return <div>loading...</div>        
         }
+        console.log(this.props.stream)
         return (
-            <div>
-                edit
-            </div>
+            
+            <>
+                <h3>
+                    Edit Stream
+                </h3>
+                <StreamForm 
+                initialValues={_.pick(this.props.stream, 'title','description')}
+                onSubmit={this.onSubmit}
+                />
+            </>
         );
     }
 }
 const mapStateToProps = (state,ownProps) => {
     return {stream:state.streams[ownProps.match.params.id]}
 }
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
