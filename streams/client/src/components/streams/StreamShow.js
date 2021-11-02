b38053e3119fb40a45feb297842b3384c619059e
ariakhayer <ariakh110@gmail.com>
2021-11-02 11:43:22 +0330

    v-89 conf flv.js

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -10,9 +15,10 @@ class StreamShow extends Component {
     if (!this.props.stream) {
       return <div>loading...</div>;
     }
-    const { title, description } = this.props.stream;//use this to block repeat
+    const { title, description } = this.props.stream; //use this to block repeat
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h2>{description}</h2>
       </div>
