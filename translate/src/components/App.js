2812f8bca682d19283a94018eeaec9d1b287b82a
ariakhayer <ariakh110@gmail.com>
2021-11-03 18:21:12 +0330

    v-104 how to use context insted of redux

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from "react";
 import ColorContext from "../contexts/ColorContext";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import LanguageSelector from "./LanguageSelector";
 import UserCreate from "./UserCreate";
 
 class App extends Component {
-  state = {
-    language: "fa",
-    color: "green"
-  };
-  onLanguageChange = (language, color) => {
-    this.setState({ language ,color });
-  };
   render() {
     return (
       <div className="ui container">
-      <LanguageSelector onLanguageChange={this.onLanguageChange}/>
-        <ColorContext.Provider value={this.state.color}>
-          <LanguageContext.Provider value={this.state.language}>
-            <UserCreate></UserCreate>
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value={this.props.color}>
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
--- translate/src/components/LanguageSelector.js
+++ translate/src/components/LanguageSelector.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
-
+import LanguageContext from "../contexts/LanguageContext"
 export class LanguageSelector extends Component {
+    static contextType = LanguageContext;
   render() {
     return (
       <div>
         <h3>select a language:</h3>
         <i
           className="flag us"
-          onClick={() => this.props.onLanguageChange("en", "red")}
+          onClick={() => this.context.onLanguageChange("en", "red")}
         />
         <i
           className="flag ir"
-          onClick={() => this.props.onLanguageChange("fa", "green")}
+          onClick={() => this.context.onLanguageChange("fa", "green")}
         />
       </div>
     );
