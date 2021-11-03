9b12feb614752f2b131eb6084bbacbb21d166884
ariakhayer <ariakh110@gmail.com>
2021-11-03 11:58:55 +0330

    v-100 use consumer in button

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import LanguageContext from '../contexts/LanguageContext';
+import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-
-    static contextType = LanguageContext;
-
-    render() {
-        const text = this.context === 'en' ? 'Submit' : 'کلیک';
-        return (
-            <button className="ui button primary">
-                {text}
-            </button>
-        );
-    }
+  //1.provider
+  //static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "en" ? "Submit" : "کلیک";
+  }
+  render() {
+    // const text = this.context === "en" ? "Submit" : "کلیک";
+    return (
+      <button className="ui button primary">
+        {/* 2. consumers */}
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
 }
 
 export default Button;
