14efe4ecd94b773a92d8a35603d7640e27d5030f
ariakhayer <ariakh110@gmail.com>
2021-11-04 19:11:40 +0330

    v-113 fix todos

--- hooks/src/components/ResourceList.jsx
+++ hooks/src/components/ResourceList.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from "axios";
 class ResourceList extends Component {
   state = {
     resource: [],
   };
-   async componentDidMount() {
-      const resource = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-      this.setState({resource: resource.data})
+  async componentDidMount() {
+    const resource = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+    this.setState({ resource: resource.data });
   }
-  
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevProps.resource !== this.props.resource) {
+      const resource = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState({ resource: resource.data });
+    }
+  }
+
   render() {
     return <h2>{this.state.resource.length}</h2>;
   }
