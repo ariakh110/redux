37c24ed3f01cc71371575827bb0692c93ae6e0af
ariakhayer <ariakh110@gmail.com>
2021-11-06 14:40:18 +0330

    v-114 refactor to fn base

--- hooks/src/components/ResourceList.jsx
+++ hooks/src/components/ResourceList.jsx
@@ -1,27 +1,14 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-class ResourceList extends Component {
-  state = {
-    resource: [],
-  };
-  async componentDidMount() {
-    const resource = await axios.get(
+const ResourceList = () => {
+  const [resources, setResources] = useState([]);
+  const fetchResource = async () => {
+    const { data } = await axios.get(
       `https://jsonplaceholder.typicode.com/${this.props.resource}`
     );
-    this.setState({ resource: resource.data });
-  }
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.resource !== this.props.resource) {
-      const resource = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState({ resource: resource.data });
-    }
-  }
-
-  render() {
-    return <h2>{this.state.resource.length}</h2>;
-  }
-}
+    this.setState({ resources: data });
+  };
+  return <h2>{resources.length}</h2>;
+};
 
 export default ResourceList;
