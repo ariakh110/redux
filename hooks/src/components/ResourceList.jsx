68d413c2a82fb2f0e35bdc871d79d707ad4d5bd7
ariakhayer <ariakh110@gmail.com>
2021-11-06 14:49:45 +0330

    v-115 use {useEffect}insted of cdu

--- hooks/src/components/ResourceList.jsx
+++ hooks/src/components/ResourceList.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
-  const fetchResource = async () => {
+
+  const fetchResource = async (resource) => {
     const { data } = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState({ resources: data });
+    setResources(data);
   };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+
   return <h2>{resources.length}</h2>;
 };
 
